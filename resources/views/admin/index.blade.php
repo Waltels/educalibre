f48c2457362fab677e380288f24b3e71ed0b6231
Walter Laura Soto <waltels20219@gmail.com>
2021-10-06 22:18:47 -0400

    terminado hasta plantilla admin y jetstram

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,19 @@
+@extends('adminlte::page')
+
+@section('title', 'educaLibre')
+
+@section('content_header')
+    <h1>Escritorio</h1>
+@stop
+
+@section('content')
+    <p>BIen venido al panel de administracion de educaLibre.</p>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
---
+++ routes/admin.php
@@ -0,0 +1,7 @@
+<?php
+
+use App\Http\Controllers\admin\HomeController;
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('', [HomeController::class, 'index']);
